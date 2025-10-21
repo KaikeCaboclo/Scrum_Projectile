1fa883ce0ab549ce5a45d07d2108eac0f6f9727b
Kaike Caboclo <kaikelima608@gmail.com>
2025-10-21 17:44:53 -0300

    <feat.> adição de funcionalidade das checkboxes da sidebar, carregamento dinâmico de conteúdos e melhoria na página módulos com JS nas divs

--- Projeto Principal/static/modulos.js
+++ Projeto Principal/static/modulos.js
@@ -1,4 +1,4 @@
-// modulos.js - Navegação entre páginas do módulo
+/*// modulos.js - Navegação entre páginas do módulo
 const paginasModulo1 = [
     "modulo1",
     "oquee_scrum", 
@@ -58,4 +58,76 @@ function setupNavigation() {
 }
 
 // Executa quando a página carrega
-document.addEventListener('DOMContentLoaded', setupNavigation);
+document.addEventListener('DOMContentLoaded', setupNavigation);*/
+
+
+
+
+/*Cards módulos (abrir link ao clicar na div)*/
+const rota_modulos=window.location.pathname.split('/').pop()
+if(rota_modulos==='modulos'){
+    document.querySelectorAll('.modulo-card').forEach(modulo=>{
+        modulo.addEventListener('click', redirect)
+    })
+}
+
+function redirect(abrir){
+    const abrir_modulo=abrir.currentTarget.dataset.moduloconteudo;
+    window.location.href=abrir_modulo
+
+}
+
+/*-----------------------------------------------------------------*/
+
+
+
+
+
+/*abrir conteúdos dos módulos*/
+var rotasdosmodulos=['modulo1', 'modulo2', 'modulo3', 'modulo4', 'modulo5', 'modulo6', 'modulo7', 'modulo8', 'modulo9']
+const rota_do_modulo=window.location.pathname.split('/').pop()
+if(rotasdosmodulos.includes(rota_do_modulo)){
+    document.querySelectorAll('.navegacacao_conteudo').forEach(botao=>{
+        botao.addEventListener('click', mudarconteudo)
+    })
+}
+
+async function mudarconteudo(botao) {
+     if (botao.target.tagName === 'INPUT') {
+        return;
+    }
+    const local=botao.currentTarget.dataset.conteudos;
+    botao.preventDefault()
+    try{
+        const pegar= await fetch(local)
+        const transformaTXT= await pegar.text()
+        document.querySelector('.artigo').innerHTML=transformaTXT;
+     } catch(erro){
+        console.error(erro);
+     }
+    
+}
+/*----------------------------------------------------------------------------------------*/
+
+/*Checkboxes de progresso*/
+if(rotasdosmodulos.includes(rota_do_modulo)){
+    document.querySelectorAll('input[type="checkbox"]').forEach(caixa=>{
+        caixa.addEventListener('change', guardarAvanco)
+    })
+
+    if(localStorage.getItem(document.querySelector('input[type="checkbox"]').name)){
+        JSON.parse(localStorage.getItem(document.querySelector('input[type="checkbox"]').name)).forEach(value=>{
+            document.querySelectorAll('input[type="checkbox"][value="'+value+'"]').forEach(input=>input.checked=true)
+        })
+    }
+}
+
+function guardarAvanco(input){
+    const nome=input.currentTarget.name
+    const listaMarcados=[]
+    document.querySelectorAll('input[name="'+nome+'"]:checked').forEach(marcado=>{
+        listaMarcados.push(marcado.value)
+    })
+    localStorage.setItem(nome, JSON.stringify(listaMarcados))
+    
+}
